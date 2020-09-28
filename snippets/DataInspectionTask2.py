f218a3e76de6ea9d5db80e8ea5fb7e360f009f48
Michael Pitt <m_u_pitt@yahoo.com>
2020-09-28 12:10:45 +0200

    Update DataInspectionTask2.py

--- snippets/DataInspectionTask2.py
+++ snippets/DataInspectionTask2.py
@@ -3,7 +3,7 @@ def ComputeXi(dataframe):
     """ computes the proton csi based on the central system kinematics"""
 
     sqrts=13000.
-    dataframe['recXi_pos'] = (1./sqrts)*(dataframe['Muon0Pt']*np.exp(dataframe['Muon0Eta'])+dataframe['Muon1Pt']*np.exp(dataframe['Muon1Eta']))
-    dataframe['recXi_neg'] = (1./sqrts)*(dataframe['Muon0Pt']*np.exp(-dataframe['Muon0Eta'])+dataframe['Muon1Pt']*np.exp(-dataframe['Muon1Eta']))
+    dataframe['recXi_pos'] = (1./sqrts)*(dataframe['Lep0Pt']*np.exp(dataframe['Lep0Eta'])+dataframe['Lep1Pt']*np.exp(dataframe['Lep1Eta']))
+    dataframe['recXi_neg'] = (1./sqrts)*(dataframe['Lep0Pt']*np.exp(-dataframe['Lep0Eta'])+dataframe['Lep1Pt']*np.exp(-dataframe['Lep1Eta']))
     
     return dataframe
